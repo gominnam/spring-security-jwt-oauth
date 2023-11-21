4d3bb6cd5f38d3ebe78dc9f5ff5158ee8288280e
gominnam <kosok03@naver.com>
2023-11-21 23:45:48 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/springsecurityjwtoauth/SpringSecurityJwtOauthApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springsecurityjwtoauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityJwtOauthApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringSecurityJwtOauthApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springsecurityjwtoauth/SpringSecurityJwtOauthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springsecurityjwtoauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityJwtOauthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
