6d588b4fb68475224b8f2d4174f0d6183fb4b90c
gominnam <kosok03@naver.com>
2023-11-30 15:36:03 +0900

    add oauth2 dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,9 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
+    /*SECURITY - OAUTH2*/
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client:2.6.2")
+
     /*DATABASE*/
     runtimeOnly("com.h2database:h2")
 
